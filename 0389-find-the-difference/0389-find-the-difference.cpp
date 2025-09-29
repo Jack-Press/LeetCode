9346f87621f98fc7950a40bca92ed4320199e650
Jack Press <Jackpress6@gmail.com>
2025-09-29 14:43:04 -0500

    Time: 0 ms (100%), Space: 10.2 MB (9.37%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> chars;
+        chars.reserve(s.size());
+
+        for(char c : s){
+            if(chars.count(c)){
+                chars[c]++;
+            }
+            else{
+                chars[c] = 1;
+            }
+        }
+
+        for(char c : t){
+            if(chars.count(c) && chars[c] != 0){
+                chars[c]--;
+            }
+            else{
+                return c;
+            }
+        }
+
+        return 'a';
+    }
+};
