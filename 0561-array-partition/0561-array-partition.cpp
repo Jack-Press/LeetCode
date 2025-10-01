067fa6f12fcf0a2b68fcec509904f083b042d333
Jack Press <Jackpress6@gmail.com>
2025-10-01 15:49:48 -0500

    Time: 26 ms (5.03%), Space: 31.9 MB (81.9%) - LeetHub

--- 0561-array-partition/0561-array-partition.cpp
+++ 0561-array-partition/0561-array-partition.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
+        int sum = nums[0];
         for(int i = 2; i < nums.size(); i += 2){
-            nums[0] += nums[i];
+            sum += nums[i];
         }
-        return nums[0];
+        return sum;
     }
 };
