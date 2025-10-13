b04d3051fa1e0c5c5ceb8678ab5f0a6a09d2a6b4
Jack Press <Jackpress6@gmail.com>
2025-10-13 14:26:16 -0500

    Time: 0 ms (100%), Space: 16.7 MB (37.57%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -9,18 +9,23 @@ public:
         vector<string> res;
         res.reserve(words.size());
         res.push_back(words[0]);
+        vector<uint8_t> next(26, 0);
 
         for (int i = 1; i < words.size(); i++) {
-            vector<uint8_t> next(26, 0);
+            for(int i = 0; i < next.size(); i++){
+                next[i] = 0;
+            }
+
             for(char c : words[i]){
                 next[c - 'a']++;
             }
+
             if(curr != next){
                 res.push_back(words[i]);
                 curr = next;
             }
         }
-        
+
         return res;
     }
 };
