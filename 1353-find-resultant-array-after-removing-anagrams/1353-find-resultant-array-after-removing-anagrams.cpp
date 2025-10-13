7b4bd3891bd8a007ef181bd5799a01a26cf33e3d
Jack Press <Jackpress6@gmail.com>
2025-10-13 14:21:13 -0500

    Time: 3 ms (48.84%), Space: 17.4 MB (25.43%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<string> removeAnagrams(vector<string>& words) {
-        vector<unordered_map<char, int>> freq(words.size());
-
-        for (int i = 0; i < words.size(); i++) {
-            for (char ch : words[i]){
-                freq[i][ch]++;
-            }
+        vector<uint8_t> curr(26, 0);
+        for(char c : words[0]){
+            curr[c - 'a']++;
         }
 
         vector<string> res;
         res.reserve(words.size());
         res.push_back(words[0]);
+
         for (int i = 1; i < words.size(); i++) {
-            if (freq[i] != freq[i - 1]){
+            vector<uint8_t> next(26, 0);
+            for(char c : words[i]){
+                next[c - 'a']++;
+            }
+            if(curr != next){
                 res.push_back(words[i]);
+                curr = next;
             }
         }
+        
         return res;
     }
 };
