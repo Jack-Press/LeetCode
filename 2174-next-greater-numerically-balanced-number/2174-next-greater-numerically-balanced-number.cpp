d9db60b6a06bc4cff125a3d00670df76456012b4
Jack Press <Jackpress6@gmail.com>
2025-10-27 14:26:58 -0500

    Time: 0 ms (100%), Space: 8.4 MB (78.63%) - LeetHub

--- 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -1,37 +1,45 @@
+static vector<int> Bal;
+static int tens[]={1,10,100,1000,10000,100000,1000000};
+
 class Solution {
 public:
-    int nextBeautifulNumber(int n) {
-        vector<int> list;
-        vector<int> count(10, 0);
-        generate(0, count, list);
-        sort(list.begin(), list.end());
-        for (int num : list) {
-            if (num > n) return num;
+    static void permDigits(vector<int>& digit){
+        const int dz=digit.size();
+        if (dz==1){// O(|digit|)
+            int x=0, d=digit[0];
+            for(int i=0;i<d;i++) x=10*x+d;
+            Bal.push_back(x);
+            return;
         }
-        return -1;
+        //  the following takes O(|permuations||digit|) 
+        string s="";
+        for(int d : digit) s.insert(s.end(), d, '0'+d);
+        do{
+            int x=stoi(s);
+            if (x<=1224444) Bal.push_back(x);
+        }while(next_permutation(s.begin(), s.end()));
     }
 
-private:
-    void generate(long num, vector<int>& count, vector<int>& list) {
-        if (num > 0 && isBeautiful(count)) {
-            list.push_back((int)num);
-        }
-        if (num > 1224444) return;
-
-        for (int d = 1; d <= 7; ++d) {
-            if (count[d] < d) {
-                count[d]++;
-                generate(num * 10 + d, count, list);
-                count[d]--;
+    static void genBalanced(){
+        if (!Bal.empty()) return;
+        for (unsigned mask=1; mask<(1u<<6); mask++){
+            int len=0;
+            vector<int> digit;
+            for (int d=0; d<6; d++){
+                if (mask&(1<<d)){
+                    len+=d+1;
+                    if (len>7) break;
+                    digit.push_back(d+1);
+                }
             }
+            if (len<=7)
+                permDigits(digit);
         }
+        sort(Bal.begin(), Bal.end());
     }
 
-    bool isBeautiful(const vector<int>& count) {
-        for (int d = 1; d <= 7; ++d) {
-            if (count[d] != 0 && count[d] != d)
-                return false;
-        }
-        return true;
+    static int nextBeautifulNumber(int n){
+        genBalanced();
+        return *upper_bound(Bal.begin(), Bal.end(), n);
     }
 };
